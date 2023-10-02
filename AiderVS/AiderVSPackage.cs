2610e64197e99b4ee8936ffd29170c37da113026
Oleg Rakovitch <devgeslot@gmail.com>
2023-10-02 19:51:14 -0400

    Log window for Aider output

--- AiderVS/AiderVSPackage.cs
+++ AiderVS/AiderVSPackage.cs
@@ -1,56 +1,64 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Task = System.Threading.Tasks.Task;
+using System.Threading.Tasks;
 
-namespace AiderVS
+namespace AiderVS;
+
+/// <summary>
+/// This is the class that implements the package exposed by this assembly.
+/// </summary>
+/// <remarks>
+/// <para>
+/// The minimum requirement for a class to be considered a valid package for Visual Studio
+/// is to implement the IVsPackage interface and register itself with the shell.
+/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+/// to do it: it derives from the Package class that provides the implementation of the
+/// IVsPackage interface and uses the registration attributes defined in the framework to
+/// register itself and its components with the shell. These attributes tell the pkgdef creation
+/// utility what data to put into .pkgdef file.
+/// </para>
+/// <para>
+/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+/// </para>
+/// </remarks>
+[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+[Guid(PackageGuidString)]
+[ProvideMenuResource("Menus.ctmenu", 1)]
+public sealed class AiderVSPackage : AsyncPackage
 {
     /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
+    /// AiderVSPackage GUID string.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [Guid(AiderVSPackage.PackageGuidString)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
-    public sealed class AiderVSPackage : AsyncPackage
+    public const string PackageGuidString = "1c8a04a1-7e9f-413d-b258-7303da93b38e";
+
+    OutputWindowLogger Logger;
+
+    /// <summary>
+    /// Initialization of the package; this method is called right after the package is sited, so this is the place
+    /// where you can put all the initialization code that rely on services provided by VisualStudio.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+    /// <param name="progress">A provider for progress updates.</param>
+    /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
     {
-        /// <summary>
-        /// AiderVSPackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "1c8a04a1-7e9f-413d-b258-7303da93b38e";
+        // When initialized asynchronously, the current thread may be a background thread at this point.
+        // Do any initialization that requires the UI thread after switching to the UI thread.
+        await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        await AddToChatCommand.InitializeAsync(this);
+        await RemoveFromChatCommand.InitializeAsync(this);
+        var window = (IVsOutputWindow)await GetServiceAsync(typeof(SVsOutputWindow));
+        Logger = await OutputWindowLogger.InitializeAsync(this, window, new Guid("75b97a3e-005b-448b-bca5-180710799bac"), "Aider");
+        Logger.Activate();
+        Logger.LogMessage("Aider loaded");
+    }
 
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-            // When initialized asynchronously, the current thread may be a background thread at this point.
-            // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await AddToChatCommand.InitializeAsync(this);
-            await RemoveFromChatCommand.InitializeAsync(this);
-        }
-
-        #endregion
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
     }
 }
---
+++ AiderVS/OutputWindowLogger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace AiderVS
+{
+    public class OutputWindowLogger
+    {
+        readonly IVsOutputWindowPane Pane;
+
+        public OutputWindowLogger(IVsOutputWindowPane pane)
+            => Pane = pane;
+
+        public static async Task<OutputWindowLogger> InitializeAsync(AsyncPackage package, IVsOutputWindow window, Guid paneGuid, string paneTitle)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            if (window.GetPane(ref paneGuid, out var pane) != VSConstants.S_OK)
+            {
+                window.CreatePane(ref paneGuid, paneTitle, fInitVisible: 1, fClearWithSolution: 0);
+                window.GetPane(ref paneGuid, out pane);
+            }
+            return new(pane);
+        }
+
+        public void LogMessage(string message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            Pane.OutputStringThreadSafe(message + Environment.NewLine);
+        }
+
+        public void Activate()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            Pane.Activate();
+        }
+    }
+}
